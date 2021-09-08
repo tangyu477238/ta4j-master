6c60453993a80bda2d4db878c22a62151f194380
tangzhiyu <tangzhiyu@vldtech.com>
2021-09-08 09:10:41 +0800

    init

--- platform/src/main/java/com/chuancai/tfish/manager/impl/GupiaoXinhaoManagerImpl.java
+++ platform/src/main/java/com/chuancai/tfish/manager/impl/GupiaoXinhaoManagerImpl.java
@@ -36,7 +36,9 @@ public class GupiaoXinhaoManagerImpl implements GupiaoXinhaoManager {
     @Override
     public void saveGupiaoXinhao(List<GupiaoXinhao> list) {
         List<GupiaoXinhao> addList = new ArrayList();
-
+        if (ComUtil.isEmpty(list.isEmpty())){
+            return;
+        }
         String maxBizDate = gupiaoXinhaoRepository.getMaxBizDate(list.get(0).getSymbol(),list.get(0).getPeriod(),"zjrc");
         for (GupiaoXinhao gupiaoXinhao : list) {
             if (!ComUtil.isEmpty(maxBizDate)
@@ -45,33 +47,10 @@ public class GupiaoXinhaoManagerImpl implements GupiaoXinhaoManager {
             }
             addList.add(gupiaoXinhao);
         }
-
-//          Date date1 = new Date();
-//        for (int i = 0; i < list.size(); i++) {
-//            GupiaoXinhao gupiaoXinhao = list.get(i);
-//            if (!ComUtil.isEmpty(maxBizDate)
-//                    && gupiaoXinhao.getBizDate().compareTo(maxBizDate) <= 0){ //已经存在的信号,不在计算和验证
-//                continue;
-//            }
-//            GupiaoXinhao gupiaoXinhao_1  = gupiaoXinhaoRepository.findBySymbolAndTypeNameAndBizDateAndPeriod(gupiaoXinhao.getSymbol(),
-//                    gupiaoXinhao.getTypeName(), gupiaoXinhao.getBizDate(), gupiaoXinhao.getPeriod());
-//            if (ComUtil.isEmpty(gupiaoXinhao_1)){
-//                addList.add(gupiaoXinhao);
-//                continue;
-//            }
-//            if (gupiaoXinhao.getBizDate().startsWith(DateTimeUtil.getBeforeDay(0))) { //如果是当天，请覆盖
-//                gupiaoXinhao.setId(gupiaoXinhao_1.getId());
-//                addList.add(gupiaoXinhao);
-//                continue;
-//            }
-//        }
-//        log.info("--------验证数-----"+DateTimeUtil.getSecondsOfTwoDate(date1,new Date())+"");date1 = new Date();
         if (ComUtil.isEmpty(addList)){
             return;
         }
         gupiaoXinhaoRepository.saveAll(addList);
-//        log.info("--------存数-----"+DateTimeUtil.getSecondsOfTwoDate(date1,new Date())+"");
-
     }
 
 
@@ -83,6 +62,7 @@ public class GupiaoXinhaoManagerImpl implements GupiaoXinhaoManager {
               Runnable run = new GupiaoXinhaoManagerImpl.GupiaoXinhaoAllRunnable(gupiao, period);
               ExecutorProcessPool.getInstance().executeByFixedThread(run);
             } catch (Exception e){
+                log.info(gupiao.getSymbol());
                 e.printStackTrace();
             }
         }
